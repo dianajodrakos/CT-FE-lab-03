301f7b85a18246587a55c580a0399b3d27699127
DJ Drakos <dianajodrakos@gmail.com>
2021-09-10 09:52:31 -0700

    created empty test and container component

---
+++ src/containers/NewsContainer.test.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { render, screen } from '@testing-library/react';
+
+describe('NewsContainer', () => {
+  it('should display a list of news articles', async () => {
+
+  });
+});
