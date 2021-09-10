f63ee6e260889e0101836dd8bc868ce2efc74387
DJ Drakos <dianajodrakos@gmail.com>
2021-09-10 15:59:16 -0700

    changed api key name in service layer

--- src/services/newsAPI.jsx
+++ src/services/newsAPI.jsx
@@ -1,6 +1,6 @@
 export const fetchArticles = async (query) => {
   // eslint-disable-next-line max-len
-  const res = await fetch(`https://newsapi.org/v2/everything?q=${query}&apiKey=${process.env.API_KEY}`);
+  const res = await fetch(`https://newsapi.org/v2/everything?q=${query}&apiKey=${process.env.REACT_APP_API_KEY}`);
 
   const json = await res.json();
   return json.articles.map((article) => ({
