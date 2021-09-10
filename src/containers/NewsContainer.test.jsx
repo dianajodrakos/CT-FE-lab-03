9568aa338b65246e9a6f3c900d67f40a268125bd
DJ Drakos <dianajodrakos@gmail.com>
2021-09-10 09:56:24 -0700

    created failing test for container component

--- src/containers/NewsContainer.test.jsx
+++ src/containers/NewsContainer.test.jsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React from 'react';
 import { render, screen } from '@testing-library/react';
+import NewsContainer from './NewsContainer';
 
 describe('NewsContainer', () => {
   it('should display a list of news articles', async () => {
+    render(<NewsContainer />);
 
+    screen.getByText('Loading...');
+
+    const ul = await screen.findAllByRole('list', { name: 'articles' });
+    expect(ul).notToBoEmptyDOMElement();
   });
 });
